47539dcee48bc6944e4241d283ad543de113808b
Brian Lobdell <bri.lobdell@gmail.com>
2014-01-31 16:55:24 -0500

    production/development changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Emailtest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # set ti actual host name
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
